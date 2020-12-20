806a53e912b7a525e20866ee0e8bcf6c7eefca32
Henrique <Ramires>
2020-12-20 19:29:45 -0300

    Reatividade.

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,16 @@
   <div id="app">
     <h1>Guia Clientes</h1>
     <Cliente />
-    <Produto />
-    <hr />
-    <Cliente />
-    <Produto />
+
   </div>
 </template>
 
 <script>
 import Cliente from "./components/Cliente";
-import Produto from "./components/Produto";
 export default {
   name: "App",
   components: {
-    Cliente,
-    Produto
+    Cliente
   },
 };
 </script>
--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="cliente">
-    <h2>Nome</h2>
-    <h2>Idade</h2>
+    <h2>Nome: {{ nome }}</h2>
+    <h2>Idade: {{ idade }}</h2>
     <hr />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      nome: "Henrique Ramires",
+      idade: 29,
+    };
+  },
+};
 </script>
 
 <style scoped>
 #cliente {
-  max-width: 200px;
+  max-width: 340px;
   height: 120px;
   padding: 1%;
   margin-top: 2%;
